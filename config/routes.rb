cb989dfc59fd65c4f9685d7293101c2d277b7966
ChikaraShi <chikara.2000.082@gmail.com>
2020-08-24 14:55:53 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "rooms#index"
   resources :users, only: [:edit, :update]
-<<<<<<< Updated upstream
-  resources :rooms, only: [:new, :create]
-=======
->>>>>>> Stashed changes
   resources :rooms, only: [:new, :create] do
     resources :messages, only: [:index, :create]
   end
